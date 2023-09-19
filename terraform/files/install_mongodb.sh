59a697ec969d4740009f606c0743d7b7fea5b0ef
dmitriy-kutepow <dmitriy.kutepow@yandex.ru>
2023-09-19 12:43:08 +0300

    add ansible-1

---
+++ terraform/files/install_mongodb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+sudo pkill -9 apt
+sudo apt install mongodb -y
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
--- terraform/main.tf
+++ terraform/main.tf
@@ -48,12 +48,61 @@ resource "yandex_compute_instance" "app" {
     private_key = file(var.ubuntu_private_key_path)
   }
 
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
+#  provisioner "file" {
+#    source      = "files/puma.service"
+#    destination = "/tmp/puma.service"
+#  }
+
+  provisioner "remote-exec" {
+    script = "files/install_ruby.sh"
   }
 
   provisioner "remote-exec" {
-    script = "files/deploy.sh"
+    script = "files/install_reddit.sh"
+  }
+}
+
+resource "yandex_compute_instance" "db" {
+  name = "db-app"
+  zone = var.zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашем задании
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.ubuntu_private_key_path)
+  }
+
+#  provisioner "file" {
+#    source      = "files/install_mongodb.sh"
+#    destination = "/tmp/install_mongodb.sh"
+#  }
+
+  provisioner "remote-exec" {
+    script = "files/install_mongodb.sh"
   }
 }
